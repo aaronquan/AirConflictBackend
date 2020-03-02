9d37d82895292442df08496884e45adb2f9f5c4f
Aaron Quan <z5020387@zmail.unsw.edu.au>
2020-03-02 22:50:55 +1100

    Update settings

--- AirConflict/settings.py
+++ AirConflict/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '741c167d-c1c5-4ff5-9338-50674cf2fde1'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['airconflictapi.herokuapp.com']
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
